9d4af848cee6201d45d873a974500a117f9ed351
pjhellstrom <pjh.hellstrom@gmail.com>
2019-10-07 12:52:23 -0400

    Added scrapers
    Added scrapers for MacRumors and TechNewsWorld

--- routes/scrape.js
+++ routes/scrape.js
@@ -42,7 +42,85 @@ router.get("/:selection", (req, res) => {
               .children("picture")
               .children("source")
               .attr("srcset")
-              .split(" ")[0]
+              .split(" ")[0],
+            source: "TechCrunch"
+          });
+        });
+        res.json(headlines);
+      })
+      .catch(console.error);
+  } else if (selection === "macrumors") {
+    const url = "https://www.macrumors.com";
+
+    puppeteer
+      .launch()
+      .then(browser => browser.newPage())
+      .then(page => {
+        return page.goto(url).then(function() {
+          return page.content();
+        });
+      })
+      .then(html => {
+        const $ = cheerio.load(html);
+        const headlines = [];
+        $("div.article").each(function() {
+          headlines.push({
+            title: $(this)
+              .children("h2")
+              .children("a")
+              .text(),
+            body: $(this)
+              .children("div.content")
+              .children("div.content_inner")
+              .text()
+              .substring(0, 100),
+            link: $(this)
+              .children("h2")
+              .children("a")
+              .attr("href"),
+            image: $(this)
+              .children("div.content")
+              .children("div.content_inner")
+              .children("img")
+              .attr("src"),
+            source: "MacRumors"
+          });
+        });
+        res.json(headlines);
+      })
+      .catch(console.error);
+  } else if (selection === "technewsworld") {
+    const url = "https://www.technewsworld.com";
+
+    puppeteer
+      .launch()
+      .then(browser => browser.newPage())
+      .then(page => {
+        return page.goto(url).then(function() {
+          return page.content();
+        });
+      })
+      .then(html => {
+        const $ = cheerio.load(html);
+        const headlines = [];
+        $("div.story-list").each(function() {
+          headlines.push({
+            title: $(this)
+              .children("div.title")
+              .text(),
+            body: $(this)
+              .children("div.teaser")
+              .text(),
+            link: $(this)
+              .children("span.story-link")
+              .children("a")
+              .attr("href"),
+            image: $(this)
+              .children("div.image")
+              .children("a")
+              .children("img")
+              .attr("src"),
+            source: "TechNewsWorld"
           });
         });
         res.json(headlines);
